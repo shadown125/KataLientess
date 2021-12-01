503a6e013db678173587671136ffac3bc2bff329
YoLoN <shadyolown13@onet.pl>
2021-12-01 21:19:09 +0100

    Add backdrop filter for Add todo section

--- components/layout/Backdrop.js
+++ components/layout/Backdrop.js
@@ -1,5 +1,5 @@
 function Backdrop(props) {
-    return <div className={`backdrop${props.currentMainNavigationState ? ' is-active' : ''}`} />
+    return <div className={`backdrop${props.currentMainNavigationState ? ' is-active' : ''}${props.activeTodo ? ' is-active-full' : ''}`} />
 }
 
 export default Backdrop;
