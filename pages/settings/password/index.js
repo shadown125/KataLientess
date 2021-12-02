505e947a7e8d1b99d16bdfe2a34bb56309f31548
YoLoN <shadyolown13@onet.pl>
2021-12-02 18:10:31 +0100

    Add settings password page

---
+++ pages/settings/password/index.js
@@ -0,0 +1,58 @@
+import HomePage from "../../index";
+import FullActiveBackdrop from "../../../components/layout/FullActiveBackdrop";
+import {Fragment} from "react";
+import Link from "next/link";
+import Profile from "../../../components/elements/Profile";
+import {useState} from 'react';
+
+function SettingsPassword() {
+    const [enteredPassword, setEnteredPassword] = useState('');
+    const [enteredRepeatPassword, setEnteredRepeatPassword] = useState('');
+
+    const passwordChangeHandler = (event) => {
+        setEnteredPassword(event.target.value);
+    }
+
+    const repeatPasswordChangeHandler = (event) => {
+        setEnteredRepeatPassword(event.target.value);
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const passwordData = {
+            title: enteredPassword,
+            description: enteredRepeatPassword,
+            id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
+        }
+        setEnteredPassword('');
+        setEnteredRepeatPassword('');
+    }
+
+    return (
+        <Fragment>
+            <HomePage />
+            <section className="settings">
+                <Link href='/'>
+                    <a className="button button--medium icon-cross" />
+                </Link>
+                <Profile />
+                <form onSubmit={submitHandler}>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" id="password" placeholder="New password" value={enteredPassword} onChange={passwordChangeHandler}/>
+                    <label htmlFor="repeat-password">Repeat new password:</label>
+                    <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat new password" value={enteredRepeatPassword} onChange={repeatPasswordChangeHandler}/>
+                    <div className="buttons-container">
+                        <Link href="/settings">
+                            <a className="button button-primary">Back</a>
+                        </Link>
+                        <button className="button button-primary" type="submit">Save</button>
+                    </div>
+                </form>
+            </section>
+            <FullActiveBackdrop />
+        </Fragment>
+    );
+}
+
+export default SettingsPassword;
