ac60fb0b99123b46d8d0f98c300b3afddd122939
YoLoN <shadyolown13@onet.pl>
2021-12-02 18:38:56 +0100

    Change password form input managing to Refs

--- pages/settings/password/index.js
+++ pages/settings/password/index.js
@@ -1,31 +1,27 @@
 import HomePage from "../../index";
 import FullActiveBackdrop from "../../../components/layout/FullActiveBackdrop";
-import {Fragment, useState} from "react";
+import {Fragment, useRef} from "react";
 import Link from "next/link";
 import Profile from "../../../components/elements/Profile";
 
 function SettingsPassword() {
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [enteredRepeatPassword, setEnteredRepeatPassword] = useState('');
-
-    const passwordChangeHandler = (event) => {
-        setEnteredPassword(event.target.value);
-    }
-
-    const repeatPasswordChangeHandler = (event) => {
-        setEnteredRepeatPassword(event.target.value);
-    }
+    const password = useRef();
+    const repeatPassword = useRef();
 
     const submitHandler = (event) => {
         event.preventDefault();
+
+        const enteredPassword = password.current.value;
+        const enteredRepeatPassword = repeatPassword.current.value;
 
         const passwordData = {
             title: enteredPassword,
             description: enteredRepeatPassword,
             id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
         }
-        setEnteredPassword('');
-        setEnteredRepeatPassword('');
+
+        password.current.value = '';
+        repeatPassword.current.value = '';
     }
 
     return (
@@ -38,9 +34,9 @@ function SettingsPassword() {
                 <Profile />
                 <form onSubmit={submitHandler}>
                     <label htmlFor="password">Password:</label>
-                    <input type="password" name="password" id="password" placeholder="New password" value={enteredPassword} onChange={passwordChangeHandler}/>
+                    <input type="password" name="password" id="password" placeholder="New password" ref={password}/>
                     <label htmlFor="repeat-password">Repeat new password:</label>
-                    <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat new password" value={enteredRepeatPassword} onChange={repeatPasswordChangeHandler}/>
+                    <input type="password" name="repeat-password" id="repeat-password" placeholder="Repeat new password" ref={repeatPassword}/>
                     <div className="buttons-container">
                         <Link href="/settings">
                             <a className="button button-primary">Back</a>
