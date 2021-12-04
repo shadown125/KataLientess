62b90daf8b96ca3b8258d630dd631bf07227c3cf
YoLoN <shadyolown13@onet.pl>
2021-12-04 22:41:52 +0100

    Add formik validation to name change page

--- pages/settings/name/index.js
+++ pages/settings/name/index.js
@@ -3,24 +3,56 @@ import FullActiveBackdrop from "../../../components/layout/FullActiveBackdrop";
 import {Fragment, useRef} from "react";
 import Link from "next/link";
 import Profile from "../../../components/elements/Profile";
+import {Formik, Form, useField} from "formik";
+import * as yup from 'yup';
 
 function SettingsName() {
-    const nameRef = useRef();
-    const lastNameRef = useRef();
-    
-    const submitHandler = (event) => {
-        event.preventDefault();
 
-        const enteredName = nameRef.current.value;
-        const enteredLastName = lastNameRef.current.value;
+    const NameField = (props) => {
+        const [field, meta] = useField(props);
+        const errorText = meta.error && meta.touched ? meta.error : '';
+        if (errorText) {
+            return (
+                <Fragment>
+                    <label htmlFor="firstName">Change first name:</label>
+                    <input id="firstName" className="is-invalid" placeholder="Your new name" {...field}/>
+                    <div className="error-message">{errorText}</div>
+                </Fragment>
+            );
+        }
+
+        return (
+            <Fragment>
+                <label htmlFor="firstName">Change first name:</label>
+                <input id="firstName" placeholder="Your new name" {...field}/>
+            </Fragment>
+        );
+    }
+
+    const LastNameField = (props) => {
+        const [field] = useField(props);
+        return (
+            <Fragment>
+                <label htmlFor="lastName">Change last name:</label>
+                <input type="input" placeholder="Your new last name" id="lastName" {...field} />
+            </Fragment>
+        );
+    }
+
+    const validationSchema = yup.object({
+        firstName: yup.string().required("First name is a required field").max(30, "First name must be at most 30 characters"),
+    })
+    
+    const submitHandler = (data, {setSubmitting, resetForm}) => {
+        setSubmitting(true);
 
         const nameData = {
-            firstName: enteredName,
-            LastName: enteredLastName,
+            ...data,
             id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
         }
-        nameRef.current.value = '';
-        lastNameRef.current.value = '';
+
+        setSubmitting(false);
+        resetForm(true);
     }
     
     return (
@@ -31,18 +63,20 @@ function SettingsName() {
                     <a className="button button--medium icon-cross" />
                 </Link>
                 <Profile />
-                <form onSubmit={submitHandler}>
-                    <label htmlFor="first-name">Change first name:</label>
-                    <input type="text" name="first-name" id="first-name" placeholder="Your new name" ref={nameRef}/>
-                    <label htmlFor="last-name">Change last name:</label>
-                    <input type="text" name="last-name" id="last-name" placeholder="Your new last name" ref={lastNameRef}/>
-                    <div className="buttons-container">
-                        <Link href="/settings">
-                            <a className="button button-primary">Back</a>
-                        </Link>
-                        <button className="button button-primary" type="submit">Save</button>
-                    </div>
-                </form>
+                <Formik initialValues={{ firstName: '', lastName: '' }} onSubmit={submitHandler} validationSchema={validationSchema} >
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <NameField type="input" name="firstName" />
+                            <LastNameField name="lastName" />
+                            <div className="buttons-container">
+                                <Link href="/settings">
+                                    <a className="button button-primary">Back</a>
+                                </Link>
+                                <button className="button button-primary" disabled={isSubmitting} type="submit">Save</button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </section>
             <FullActiveBackdrop />
         </Fragment>
