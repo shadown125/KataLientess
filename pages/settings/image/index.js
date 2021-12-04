258cc384dfd1a8bccc67c05a05a152b502ef3709
YoLoN <shadyolown13@onet.pl>
2021-12-04 23:03:02 +0100

    Add formik validation to change image page

--- pages/settings/image/index.js
+++ pages/settings/image/index.js
@@ -1,21 +1,48 @@
 import HomePage from "../../index";
 import FullActiveBackdrop from "../../../components/layout/FullActiveBackdrop";
-import {Fragment, useRef} from "react";
+import {Fragment} from "react";
 import Link from "next/link";
 import Profile from "../../../components/elements/Profile";
+import {Formik, Form, useField} from "formik";
+import * as yup from 'yup';
 
 function SettingsImage() {
-    const imageInputRef = useRef();
 
-    const submitHandler = (event) => {
-        event.preventDefault();
+    const validationSchema = yup.object({
+        image: yup.string().required(),
+    })
 
-        const enteredImage = imageInputRef.current.value;
+    const ImageField = (props) => {
+        const [field, meta] = useField(props);
+        const errorText = meta.error && meta.touched ? meta.error : '';
+        if (errorText) {
+            return (
+                <Fragment>
+                    <label htmlFor="image">Change profile image:</label>
+                    <input type="file" id="image" className="is-invalid" {...field} />
+                    <div className="error-message">{errorText}</div>
+                </Fragment>
+            );
+        }
+        return (
+            <Fragment>
+                <label htmlFor="image">Change profile image:</label>
+                <input type="file" id="image" {...field} />
+            </Fragment>
+        );
+    }
+
+
+    const submitHandler = (data, {setSubmitting, resetForm}) => {
+        setSubmitting(true);
 
         const imageData = {
-            image: enteredImage,
+            ...data,
+            id: Math.floor(Math.random() * Math.floor(Math.random() * Date.now())),
         }
-        imageInputRef.current.value = '';
+
+        setSubmitting(false);
+        resetForm(true);
     }
 
     return (
@@ -26,16 +53,19 @@ function SettingsImage() {
                     <a className="button button--medium icon-cross" />
                 </Link>
                 <Profile />
-                <form onSubmit={submitHandler}>
-                    <label htmlFor="image">Change profile image:</label>
-                    <input type="file" name="image" id="image" ref={imageInputRef} />
-                    <div className="buttons-container">
-                        <Link href="/settings">
-                            <a className="button button-primary">Back</a>
-                        </Link>
-                        <button className="button button-primary" type="submit">Save</button>
-                    </div>
-                </form>
+                <Formik initialValues={{ image: ''}} onSubmit={submitHandler} validationSchema={validationSchema}>
+                    {({ isSubmitting }) => (
+                        <Form>
+                            <ImageField name="image" />
+                            <div className="buttons-container">
+                                <Link href="/settings">
+                                    <a className="button button-primary">Back</a>
+                                </Link>
+                                <button className="button button-primary" disabled={isSubmitting} type="submit">Save</button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
             </section>
             <FullActiveBackdrop />
         </Fragment>
