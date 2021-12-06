2a36a73fb92ccd83b3137168c30694334dfc55f9
YoLoN <shadyolown13@onet.pl>
2021-12-06 18:51:32 +0100

    Refactor settings image input

--- pages/settings/image/index.js
+++ pages/settings/image/index.js
@@ -6,33 +6,13 @@ import Profile from "../../../components/elements/Profile";
 import {Formik, Form, useField} from "formik";
 import * as yup from 'yup';
 import {getSession} from "next-auth/react";
+import ImageField from "../../../components/inputs/ImageField";
 
 function SettingsImage() {
 
     const validationSchema = yup.object({
         image: yup.string().required(),
     })
-
-    const ImageField = (props) => {
-        const [field, meta] = useField(props);
-        const errorText = meta.error && meta.touched ? meta.error : '';
-        if (errorText) {
-            return (
-                <Fragment>
-                    <label htmlFor="image">Change profile image:</label>
-                    <input type="file" id="image" className="is-invalid" {...field} />
-                    <div className="error-message">{errorText}</div>
-                </Fragment>
-            );
-        }
-        return (
-            <Fragment>
-                <label htmlFor="image">Change profile image:</label>
-                <input type="file" id="image" {...field} />
-            </Fragment>
-        );
-    }
-
 
     const submitHandler = (data, {setSubmitting, resetForm}) => {
         setSubmitting(true);
