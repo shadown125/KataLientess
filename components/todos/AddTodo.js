65fe9b3424bcb5e6de8217139ecb5e27ca01e113
YoLoN <shadyolown13@onet.pl>
2021-12-06 19:42:44 +0100

    Refactor inputs on Add todo component

--- components/todos/AddTodo.js
+++ components/todos/AddTodo.js
@@ -2,6 +2,8 @@ import {useState, Fragment} from 'react';
 import Notification from "../elements/Notification";
 import {Formik, Form, useField} from "formik";
 import * as yup from 'yup';
+import TitleField from "../inputs/TitleField";
+import TextareaField from "../inputs/TextareaField";
 
 function AddTodo (props) {
     const [notActive, setNotActive] = useState(false);
@@ -12,36 +14,6 @@ function AddTodo (props) {
         setSubmitted(false);
 
         props.removeActive(notActive);
-    }
-
-    const TitleField = (props) => {
-        const [field, meta] = useField(props);
-        const errorText = meta.error && meta.touched ? meta.error : '';
-        if (errorText) {
-            return (
-                <Fragment>
-                    <label htmlFor="title">Title:*</label>
-                    <input placeholder="Title for your task" className="title is-invalid" id="title" {...field} />
-                    <div className="error-message">{errorText}</div>
-                </Fragment>
-            );
-        }
-        return (
-            <Fragment>
-                <label htmlFor="title">Title:*</label>
-                <input placeholder="Title for your task" id="title" {...field} />
-            </Fragment>
-        );
-    }
-
-    const TextareaField = (props) => {
-        const [field] = useField(props);
-        return (
-            <Fragment>
-                <label htmlFor="description">Description:</label>
-                <textarea id="description" placeholder="Description text for your Task (optional)" {...field} />
-            </Fragment>
-        );
     }
 
     const validationSchema = yup.object({
@@ -73,7 +45,7 @@ function AddTodo (props) {
                 <Formik initialValues={{ title: '', description: '' }} onSubmit={submitHandler} validationSchema={validationSchema}>
                     {({ isSubmitting }) => (
                         <Form>
-                            <TitleField type="input" name="title" />
+                            <TitleField name="title" />
                             <TextareaField name="description" />
                             <button className="button button-primary" disabled={isSubmitting} type="submit">Add Todo</button>
                         </Form>
