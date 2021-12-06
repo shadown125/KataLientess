a64b2f4da0a11798fde6edaa6840f46c4b38c65b
YoLoN <shadyolown13@onet.pl>
2021-12-06 18:49:33 +0100

    Outsource name input to own component

---
+++ components/inputs/NameField.js
@@ -0,0 +1,25 @@
+import {useField} from "formik";
+import {Fragment} from "react";
+
+function NameField (props, {placeholder = 'Name', label = 'First name:*'}) {
+    const [field, meta] = useField(props);
+    const errorText = meta.error && meta.touched ? meta.error : '';
+    if (errorText) {
+        return (
+            <Fragment>
+                <label htmlFor="firstName">{label}</label>
+                <input id="firstName" className="is-invalid" placeholder={placeholder} {...field}/>
+                <div className="error-message">{errorText}</div>
+            </Fragment>
+        );
+    }
+
+    return (
+        <Fragment>
+            <label htmlFor="firstName">{label}</label>
+            <input id="firstName" placeholder={placeholder} {...field}/>
+        </Fragment>
+    );
+}
+
+export default NameField;
