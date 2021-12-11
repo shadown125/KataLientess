6a20a1f2a53e6475af2ed2720fd630639b64570d
YoLoN <shadyolown13@onet.pl>
2021-12-11 19:21:59 +0100

    Make Profile component Dynamically

--- components/elements/Profile.js
+++ components/elements/Profile.js
@@ -1,12 +1,27 @@
+import useSWR from "swr";
+import {useState, useEffect} from "react";
+
 function Profile() {
+    const [username, setUsername] = useState([]);
+
+    const {data, error} = useSWR('/api/user/getUsername', (url) => fetch(url).then(res => res.json()), { refreshInterval: 10 });
+
+    useEffect(() => {
+        if (data) {
+            setUsername(data);
+        }
+    }, [data])
+
     return (
         <div className="profile">
             <div className="image-wrapper">
                 <img src="/dummyProfileImage.jpg" alt="Profile Image"/>
             </div>
             <div className="content">
-                <div>Dawid</div>
-                <div>Ol</div>
+                <div>{username.firstName}</div>
+                {username.lastName && (
+                    <div>{username.lastName}</div>
+                )}
             </div>
         </div>
     );
