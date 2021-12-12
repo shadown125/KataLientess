45dd63cc28ade1b4144ed1fa2b87aad83b2094f2
YoLoN <shadyolown13@onet.pl>
2021-12-12 13:26:12 +0100

    Add delete done todos api

--- components/todos/DoneTodoItem.js
+++ components/todos/DoneTodoItem.js
@@ -1,11 +1,30 @@
 function DoneTodoItem(props) {
+
+    const deleteDoneTodo = async () => {
+        const response = await fetch('/api/user/deleteDoneTodo', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: props.id
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong!');
+        }
+    }
+
     return (
         <li>
             <div className="header">
                 <h3 className="headline h5">{props.title}</h3>
                 <ul className="actions">
                     <li>
-                        <button className="button icon-cross" type="button" />
+                        <button className="button icon-cross" type="button" onClick={deleteDoneTodo} />
                     </li>
                 </ul>
             </div>
