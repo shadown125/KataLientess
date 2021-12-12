756da474697780045c2d56efc9739ffd17f71508
YoLoN <shadyolown13@onet.pl>
2021-12-12 12:48:14 +0100

    Add complete todo api

--- components/todos/TodoItem.js
+++ components/todos/TodoItem.js
@@ -18,13 +18,31 @@ function TodoItem (props) {
         }
     }
 
+    const completeTodo = async () => {
+        const response = await fetch('/api/user/completeTodo', {
+            method: 'POST',
+            body: JSON.stringify({
+                id: props.id
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message || 'Something went wrong!');
+        }
+    }
+
     return (
         <li>
             <div className="header">
                 <h3 className="headline h5">{props.title}</h3>
                 <ul className="actions">
                     <li>
-                        <button className="button icon-check" type="button" />
+                        <button className="button icon-check" type="button" onClick={completeTodo} />
                     </li>
                     <li>
                         <button className="button icon-bubble" type="button" />
