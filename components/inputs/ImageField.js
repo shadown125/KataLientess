09dba799867407d1e6a97c7e7cae70a2bc110c09
YoLoN <shadyolown13@onet.pl>
2021-12-17 21:05:53 +0100

    Temporary update ImageField component to support files uploads

--- components/inputs/ImageField.js
+++ components/inputs/ImageField.js
@@ -10,7 +10,7 @@ function ImageField (props, {label = 'Change profile image*:'}) {
         return (
             <Fragment>
                 <label htmlFor="image">{label}</label>
-                <input type="file" id="image" className="is-invalid" {...field} />
+                <input type="file" id="image" className="is-invalid" name={field.name} onBlur={field.onBlur} onChange={props.onChange} />
                 <div className="error-message">{errorText}</div>
             </Fragment>
         );
@@ -19,7 +19,7 @@ function ImageField (props, {label = 'Change profile image*:'}) {
     return (
         <Fragment>
             <label htmlFor="image">{label}</label>
-            <input type="file" id="image" {...field} />
+            <input type="file" id="image" name={field.name} onBlur={field.onBlur} onChange={props.onChange} />
         </Fragment>
     );
 }
