cadccb65d968a872bee54b7bf8a07bd2b0688c8f
YoLoN <shadyolown13@onet.pl>
2021-12-18 16:07:29 +0100

    Update password validation

--- components/validationSchemas/registerValidationSchema.js
+++ components/validationSchemas/registerValidationSchema.js
@@ -1,8 +1,11 @@
 import * as yup from "yup";
 
 export const registerValidationSchema = yup.object({
-    firstName: yup.string().required("First name is a required field").max(30, "First name must be at most 30 characters"),
+    firstName: yup.string().required("First name is a required field").max(25, "First name must be at most 25 characters"),
     email: yup.string().email("Email must be a valid").required("Email is a required field"),
-    password: yup.string().required("Password is a required field").max(50, "Password must be at most 50 characters").min(6, "Password must be at least 6 characters"),
+    password: yup.string().required("Password is a required field").max(30, "Password must be at most 30 characters").min(6, "Password must be at least 6 characters").matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/,
+        "Password must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
+    ),
     repeatedPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required("Repeat password is a required field"),
 });
--- components/validationSchemas/settingsPasswordValidationSchema.js
+++ components/validationSchemas/settingsPasswordValidationSchema.js
@@ -1,6 +1,9 @@
 import * as yup from "yup";
 
 export const settingsPasswordValidationSchema = yup.object({
-    password: yup.string().required("Password is a required field").max(50, "Password must be at most 50 characters").min(6, "Password must be at least 6 characters"),
+    password: yup.string().required("Password is a required field").max(30, "Password must be at most 30 characters").min(6, "Password must be at least 6 characters").matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/,
+        "Password must Contain 8 Characters, One Uppercase, One Lowercase and One Number"
+    ),
     repeatedPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').required("Repeat password is a required field"),
 })
