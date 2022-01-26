3f16c620852ad17533f62aee09d2faacb64f4ce6
YoLoN <shadyolown13@onet.pl>
2022-01-26 20:39:32 +0100

    Refactor login validation schema

--- components/validationSchemas/loginValidationSchema.js
+++ components/validationSchemas/loginValidationSchema.js
@@ -1,6 +1,8 @@
-import * as yup from "yup";
+import {object, string} from "yup";
 
-export const loginValidationSchema = yup.object({
-    email: yup.string().email("Email must be a valid").required("Email is a required field"),
-    password: yup.string().required("Password is a required field"),
-})
+export const loginValidationSchema = () => {
+    return object({
+        email: string().email("Email must be a valid").required("Email is a required field"),
+        password: string().required("Password is a required field"),
+    })
+}
