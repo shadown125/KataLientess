a89d7eab4be7f83afc36c9c923e5261a8b657d17
YoLoN <shadyolown13@onet.pl>
2022-01-28 20:00:22 +0100

    Add mock handlers for api routes

--- mocks/handlers.js
+++ mocks/handlers.js
@@ -12,4 +12,76 @@ export const handlers = [
             )
         )
     }),
+    rest.get('/api/user/getAllTodos', (req, res, ctx) => {
+        return res(
+            ctx.json(
+                {
+                    todos: [
+                        {
+                            id: '1',
+                            title: 'First todo',
+                            description: 'This is description for first todo',
+                        },
+                        {
+                            id: '2',
+                            title: 'Second todo',
+                            description: 'This is description for second todo'
+                        }
+                    ]
+                }
+            )
+        )
+    }),
+    rest.get('/api/user/getDoneTodos', (req, res, ctx) => {
+        return res(
+            ctx.json(
+                {
+                    doneTodos: [
+                        {
+                            id: '3',
+                            title: 'First done todo',
+                            description: 'This is description for first done todos',
+                        },
+                        {
+                            id: '4',
+                            title: 'Second done todo',
+                            description: 'This is description for second done todos'
+                        }
+                    ],
+                }
+            )
+        )
+    }),
+    rest.get('/api/user/getAllTodosAndDoneTodos', (req, res, ctx) => {
+        return res(
+            ctx.json(
+                {
+                    doneTodos: [
+                        {
+                            id: '3',
+                            title: 'First done todo',
+                            description: 'This is description for first done todos',
+                        },
+                        {
+                            id: '4',
+                            title: 'Second done todo',
+                            description: 'This is description for second done todos'
+                        }
+                    ],
+                    todos: [
+                        {
+                            id: '1',
+                            title: 'First todo',
+                            description: 'This is description for first todo',
+                        },
+                        {
+                            id: '2',
+                            title: 'Second todo',
+                            description: 'This is description for second todo'
+                        }
+                    ]
+                }
+            )
+        )
+    })
 ]
