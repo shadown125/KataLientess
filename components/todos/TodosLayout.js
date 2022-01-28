382791587946fd824be92beb42ded886dc2bfa79
YoLoN <shadyolown13@onet.pl>
2022-01-28 19:54:28 +0100

    Refactor todos layout component

--- components/todos/TodosLayout.js
+++ components/todos/TodosLayout.js
@@ -1,32 +1,13 @@
 import Todos from "./Todos";
 import ProgressionBar from "./ProgressionBar";
 import DoneTodos from "./DoneTodos";
-import {useState, useEffect} from "react";
+import {useState} from "react";
 import {useRouter} from "next/router";
-import useSWR from "swr";
-import LoadingDoneTodos from "../loading-skeletons/LoadingDoneTodos";
 
 function TodosLayout(props) {
     const [activeAddTodo, setIsActiveAddTodo] = useState(true);
     const router = useRouter();
     const routerPath = router.pathname;
-    const [todosLength, setTodosLength] = useState();
-    const [doneTodos, setDoneTodos] = useState([]);
-    const [doneTodosLength, setDoneTodosLength] = useState();
-
-    const {data, error} = useSWR('/api/user/getDoneTodos', (url) => fetch(url).then(res => res.json()));
-
-    useEffect(() => {
-        if (props.allDoneTodos) {
-            setDoneTodos(props.allDoneTodos);
-            setDoneTodosLength(props.allDoneTodos.length);
-        } else {
-            if (data) {
-                setDoneTodos(data.doneTodos);
-                setDoneTodosLength(data.doneTodos.length);
-            }
-        }
-    }, [data, props]);
 
     function onSettingActiveAddTodo() {
         setIsActiveAddTodo(true);
@@ -34,46 +15,19 @@ function TodosLayout(props) {
         props.onSettingActiveAddTodo(activeAddTodo);
     }
 
-    const todosAmount = (data) => {
-        setTodosLength(() => {
-            return data;
-        });
-    }
-
-    const allTodosAfterDeleteAction = (todos) => {
-        props.allTodosAfterDeleteAction(todos);
-    }
-
-    const allTodosAfterCompleteAction = (todos) => {
-        props.allTodosAfterCompleteAction(todos);
-    }
-
-    const allDoneTodosAfterDeletingDoneTodo = (doneTodos) => {
-        props.allDoneTodosAfterDeletingDoneTodo(doneTodos);
-    }
-
-    if (!data && routerPath === '/done-todos') {
-        return (
-            <section className="todos-container">
-                <ProgressionBar doneTodosData={data} doneTodosLength={doneTodosLength} todoPage={false}/>
-                <LoadingDoneTodos />
-            </section>
-        )
-    }
-
     if (routerPath === '/done-todos') {
         return (
             <section className="todos-container">
-                <ProgressionBar doneTodosData={data} doneTodosLength={doneTodosLength} todoPage={false}/>
-                <DoneTodos doneTodos={doneTodos} allDoneTodosAfterDeletingDoneTodo={allDoneTodosAfterDeletingDoneTodo} />
+                <ProgressionBar />
+                <DoneTodos />
             </section>
         );
     }
 
     return (
         <section className="todos-container">
-            <ProgressionBar doneTodosData={data} todosAmount={todosLength} doneTodosLength={doneTodosLength} todoPage={true}/>
-            <Todos onSettingActiveAddTodo={onSettingActiveAddTodo} todosAmount={todosAmount} allTodos={props.allTodos} allTodosAfterDeleteAction={allTodosAfterDeleteAction} allTodosAfterCompleteAction={allTodosAfterCompleteAction} />
+            <ProgressionBar />
+            <Todos onSettingActiveAddTodo={onSettingActiveAddTodo} />
         </section>
     );
 }
