bc6dd22d651d2d0833836dc5fc3cbb99adb5f018
YoLoN <shadyolown13@onet.pl>
2022-01-29 15:24:30 +0100

    :hammer: Refactor settings name validation schema

--- components/validationSchemas/settingsNameValidationSchema.js
+++ components/validationSchemas/settingsNameValidationSchema.js
@@ -1,5 +1,7 @@
-import * as yup from "yup";
+import {object, string} from "yup";
 
-export const settingsNameValidationSchema = yup.object({
-    firstName: yup.string().required("First name is a required field").max(30, "First name must be at most 30 characters"),
-});
+export const settingsNameValidationSchema = () => {
+    return object({
+        firstName: string().required("First name is a required field").max(30, "First name must be at most 30 characters"),
+    });
+}
